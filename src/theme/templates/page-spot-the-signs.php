f13527f6a07f0fa4a2100e6c896087268f4b9267
James Holt <james.holt@hopeforjustice.org>
2021-11-06 08:58:10 +0000

    spot-the-signs gpf styling improvements

--- src/theme/templates/page-spot-the-signs.php
+++ src/theme/templates/page-spot-the-signs.php
@@ -8,7 +8,7 @@
 get_header();
 ?>
 
-<main id="main" class="site-main" role="main">
+<main id="main" class="site-main spot-the-signs" role="main">
 
 	<?php while ( have_posts() ) : the_post(); ?>		
 
