1df6e590b1405bc961b1cbad7ed8b7de78d14bc1
James Holt <james.holt@hopeforjustice.org>
2022-02-16 10:49:48 +0000

    freedom run

---
+++ src/theme/templates/page-freedom-run.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Freedom Run
+ *
+ * @package Hope_for_Justice_2021
+ */
+
+get_header(); ?>
+
+
+	<main id="main" class="site-main campaign" role="main">
+
+		<iframe src="https://player.vimeo.com/video/677738342?h=b8f48661d0&autoplay=1&loop=1&autopause=0&muted=1&background=1" width="640" height="640" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+
+	</main>
+
+<?php get_footer(); ?>
