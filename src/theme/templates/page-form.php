0e772810f5dc476adafbda3320ea374b8a7ac0f9
James Holt <james.holt@hopeforjustice.org>
2022-10-17 13:38:28 +0100

    fixed form page geo target bug

--- src/theme/templates/page-form.php
+++ src/theme/templates/page-form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Form page
  *
@@ -8,9 +9,9 @@
 get_header(); ?>
 
 
-	<main id="main" class="site-main form-page" role="main">
+<main id="main" class="site-main form-page" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+	<?php while (have_posts()) : the_post(); ?>
 
 		<div class="full-grid">
 			<!-- 
@@ -19,31 +20,35 @@ get_header(); ?>
 			--  
 			-->
 			<div class="form-page__content">
-				<?php 
-					if($GLOBALS['userInfo'] 
-					&& in_array($GLOBALS['userInfo'], $GLOBALS['us'])){ 
-						//US
-						echo do_shortcode( get_field('us_form') );
-					} 
-					else if($GLOBALS['userInfo'] 
-					&& in_array($GLOBALS['userInfo'], $GLOBALS['norway'])) { 
-						//Norway
-						echo do_shortcode( get_field('norway_form') );
-					}
-					else if($GLOBALS['userInfo'] 
-					&& in_array($GLOBALS['userInfo'], $GLOBALS['aus'])) {
-						//AUS
-						echo do_shortcode( get_field('aus_form') );
-					}
-					else {
-						//UK fallback
-						echo do_shortcode( get_field('uk_form') );
-					} ?>
+				<?php
+				if (
+					$GLOBALS['userInfo']
+					&& in_array($GLOBALS['userInfo'], $GLOBALS['usa'])
+				) {
+					//US
+					echo do_shortcode(get_field('us_form'));
+				} else if (
+					$GLOBALS['userInfo']
+					&& in_array($GLOBALS['userInfo'], $GLOBALS['norway'])
+				) {
+					//Norway
+					echo do_shortcode(get_field('norway_form'));
+				} else if (
+					$GLOBALS['userInfo']
+					&& in_array($GLOBALS['userInfo'], $GLOBALS['aus'])
+				) {
+					//AUS
+					echo do_shortcode(get_field('aus_form'));
+				} else {
+					//UK fallback
+					echo do_shortcode(get_field('uk_form'));
+				} ?>
 			</div>
 		</div><!-- /grid -->
 
-		<?php endwhile; // end of the loop. ?>
+	<?php endwhile; // end of the loop. 
+	?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php get_footer(); ?>
