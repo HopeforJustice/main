35ee9d8761f5e2f3626030a20272e2f18b45944c
James Holt <james.holt@hopeforjustice.org>
2022-10-17 15:15:59 +0100

    adding video to image block

--- src/theme/template-parts/blocks/image/image.php
+++ src/theme/template-parts/blocks/image/image.php
@@ -14,3 +14,21 @@ $video = get_field('video_source');
         <?php } ?>
     </div>
 </div>
+
+<?php if ($video) { ?>
+    <!--
+    --
+    --  video
+    --
+    -->
+    <div style="display: none;" class="modal modal--video fade" id="video-modal" tabindex="-1" role="dialog" aria-hidden="false">
+        <div class="modal__dialog modal__dialog--video">
+            <div class="modal__content modal__content--video video-container">
+                <iframe class="video" src="" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+
+                <a href="#" data-dismiss="modal" class="gi-close modal__close modal__close--video">&times;<span class="accessibility">Close</span></a>
+
+            </div>
+        </div>
+    </div>
+<?php } ?>
