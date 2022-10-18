f15f2b3863260cacbb034e3855bb22fc5356c416
James Holt <james.holt@hopeforjustice.org>
2022-10-18 13:52:11 +0100

    editing card thirds

--- src/theme/template-parts/blocks/cards/cards-thirds.php
+++ src/theme/template-parts/blocks/cards/cards-thirds.php
@@ -9,36 +9,29 @@
  * @param   (int|string) $post_id The post ID this block is saved to.
  */
 
-// Create id attribute allowing for custom "anchor" value.
-$id = 'cards-thirds-' . $block['id'];
-if (!empty($block['anchor'])) {
-    $id = $block['anchor'];
-}
-
 // Load values and assign defaults.
-//$count = count(get_field('third_cards')) ?: '1';
 $block_margin_bottom_desktop = get_field('block_margin_bottom_desktop') ?: '80px';
 $block_margin_bottom_mobile = get_field('block_margin_bottom_mobile') ?: '40px';
 ?>
 
 
-<div class="better-grid has-<?php echo $count ?>-cards card-thirds hfj-block" style="--margin-bottom-mobile:<?php echo $block_margin_bottom_mobile ?>; --margin-bottom-desktop: <?php echo $block_margin_bottom_desktop ?>;">
+<div class="better-grid card-thirds hfj-block" style="--margin-bottom-mobile:<?php echo $block_margin_bottom_mobile ?>; --margin-bottom-desktop: <?php echo $block_margin_bottom_desktop ?>;">
 
 
     <?php while (have_rows('third_cards')) : the_row(); ?>
         <?php
         $link_url = get_sub_field('link', ['url']) ?: "#";
         $link_target = get_sub_field('link', ['target']);
+        $image = get_sub_field('image') ?: '309';
+        $image_url = wp_get_attachment_url($image);
         ?>
         <a target="<?php echo $link_target ?>" href="<?php echo $link_url ?>" class="block-card">
-            <div class="block-card__image-container">
-                <?php $image = get_sub_field('image') ?: '309';
-                echo wp_get_attachment_image($image, 'full') ?>
+            <div style="background-image: url('<?php echo $image_url ?>');" class="block-card__image-container">
+                <div class="block-card__content">
+                    <h4 class="block-title block-card__title"><b><?php echo (get_sub_field('title')) ?></b><span class="block-card__arrow">&nbsp;<img alt="arrow" src="<?php echo get_template_directory_uri() . '/assets/img/link-arrow.svg'; ?>"></span></h4>
+                </div>
             </div>
-            <div class="block-card__content">
-                <h4 class="block-title block-card__title"><b><?php echo (get_sub_field('title')) ?></b></h4>
-                <p><?php echo (get_sub_field('text')) ?></p>
-            </div>
+
         </a>
 
     <?php endwhile; ?>
