f84b6b28787223a5e2eb3d98bc808aa0e6ef20fd
James Holt <james.holt@hopeforjustice.org>
2022-10-18 14:47:50 +0100

    cards thirds complete

--- src/theme/template-parts/blocks/cards/cards-thirds.php
+++ src/theme/template-parts/blocks/cards/cards-thirds.php
@@ -20,15 +20,31 @@ $block_margin_bottom_mobile = get_field('block_margin_bottom_mobile') ?: '40px';
 
     <?php while (have_rows('third_cards')) : the_row(); ?>
         <?php
-        $link_url = get_sub_field('link', ['url']) ?: "#";
-        $link_target = get_sub_field('link', ['target']);
-        $image = get_sub_field('image') ?: '309';
-        $image_url = wp_get_attachment_url($image);
+        $link = get_sub_field('link');
+        if ($link) {
+            $link_url = $link['url'];
+            $link_target = $link['target'];
+        } else {
+            $link_url = '#';
+            $link_target = '_self';
+        }
+        $image = get_sub_field('img');
+        if ($image) {
+            $top = $image['top'];
+            $left = $image['left'];
+            $id = $image['id'];
+        } else {
+            $top = 0;
+            $left = 0;
+            $id = '309';
+        }
+        $image_src = wp_get_attachment_image_src($id, 'full');
+        $title_size = get_sub_field('title_size');
         ?>
-        <a target="<?php echo $link_target ?>" href="<?php echo $link_url ?>" class="block-card">
-            <div style="background-image: url('<?php echo $image_url ?>');" class="block-card__image-container">
+        <a target="<?php echo $link_target ?>" <?php if (!is_admin()) { ?>href="<?php echo $link_url ?>" <?php } ?> class="block-card">
+            <div style="background-image: url('<?php echo $image_src[0]; ?>'); background-position: <?php echo $image['left'] . '% ' . $image['top']; ?>%;" class="block-card__image-container">
                 <div class="block-card__content">
-                    <h4 class="block-title block-card__title"><b><?php echo (get_sub_field('title')) ?></b><span class="block-card__arrow">&nbsp;<img alt="arrow" src="<?php echo get_template_directory_uri() . '/assets/img/link-arrow.svg'; ?>"></span></h4>
+                    <h4 class="block-title block-card__title <?php echo $title_size; ?>"><b><?php echo (get_sub_field('title')) ?></b><span style="white-space: pre;" class="block-card__arrow">&nbsp;<img alt="arrow" src="<?php echo get_template_directory_uri() . '/assets/img/link-arrow.svg'; ?>"></span></h4>
                 </div>
             </div>
 
