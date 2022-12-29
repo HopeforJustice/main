635068a2be027822c3e5486282a9316ff8fb1ba7
James Holt <james.holt@hopeforjustice.org>
2022-12-29 19:16:08 +0000

    strip tags from titles in search to remove those pesky <br> tags

--- src/theme/template-parts/content-search.php
+++ src/theme/template-parts/content-search.php
@@ -12,7 +12,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="margin-bottom:0;">
     <header class="entry-header" style="margin-bottom: 0;">
-        <h2 style="font-size: var(--wp--preset--font-size--default); font-weight:bold; margin-bottom:0;"><a href="<?php echo esc_url(get_permalink()) ?>" rel="bookmark"><?php wp_strip_all_tags(the_title()) ?></a></h2>
+        <h2 style="font-size: var(--wp--preset--font-size--default); font-weight:bold; margin-bottom:0;"><a href="<?php echo esc_url(get_permalink()) ?>" rel="bookmark"><?php strip_tags(the_title()) ?></a></h2>
     </header><!-- .entry-header -->
 
     <div>
