54a2b3e4272eb2a1e3ac45532f5d016be698c2f8
James Holt <james.holt@hopeforjustice.org>
2023-01-09 15:39:35 +0000

    updating styles

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
   // Replace the version number of the theme on each release.
-  define('_S_VERSION', '5.4.5');
+  define('_S_VERSION', '5.4.6');
 }
 
 if (!function_exists('hope_for_justice_2021_setup')) :
