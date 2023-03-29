f543dc26d6b6cbdf43a67e1f94daf1e275d0f277
James Holt <james.holt@hopeforjustice.org>
2023-03-29 16:59:05 +0100

    fixing indent images and accordions

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -13,7 +13,7 @@ function formatPostcode(string) {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-    console.log('v5.6.5')
+    console.log('v5.6.6')
     let cookies = Cookies.get('wordpress_hfjcookies');
 
     // cookieAccept click
