b6d8e8c8a7181f8422715cfe993e00243a3ea265
James Holt <james.holt@hopeforjustice.org>
2023-03-30 16:14:44 +0100

    removing allowed blocks from accordion block

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -13,7 +13,7 @@ function formatPostcode(string) {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-    console.log('v5.6.8')
+    console.log('v5.6.9')
     let cookies = Cookies.get('wordpress_hfjcookies');
 
     // cookieAccept click
--- src/theme/template-parts/blocks/accordion/accordion-content.php
+++ src/theme/template-parts/blocks/accordion/accordion-content.php
@@ -12,7 +12,6 @@
             'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         ))
     );
-    $allowed_blocks = array('core/heading', 'core/paragraph', 'core/list', 'core/spacer');
-    echo '<InnerBlocks template="' . esc_attr(wp_json_encode($template)) . '" allowedBlocks="' . esc_attr(wp_json_encode($allowed_blocks)) . '" />';
+    echo '<InnerBlocks template="' . esc_attr(wp_json_encode($template)) . '" />';
     ?>
 </div>
