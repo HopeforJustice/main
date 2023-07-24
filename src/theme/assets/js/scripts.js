d65020c3cfe2b3864a58cba33f331d412c7ea964
James Holt <james.holt@hopeforjustice.org>
2023-07-24 09:56:10 +0100

    cache

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -13,7 +13,7 @@ function formatPostcode(string) {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-    console.log('v5.8.4')
+    console.log('v5.8.5')
     let cookies = Cookies.get('wordpress_hfjcookies');
 
     // cookieAccept click
