1f46b984639ba7210bbaa4833b5fd956b41f88a7
James Holt <jamesphilipdavidholt@gmail.com>
2024-09-20 11:31:10 +0100

    Changing author in VS Code

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -12,7 +12,7 @@ function formatPostcode(string) {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.2.8");
+	console.log("v6.2.9");
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
 	// cookieAccept click
