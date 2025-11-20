7c562849ea36a4bf88b8fe49a9f51276a1f899b9
James Holt <jamesphilipdavidholt@gmail.com>
2025-11-20 15:27:31 +0000

    cache bust

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.5.0");
+	console.log("v6.5.1");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
