9abfa84ee830a52a7ce7f93170b6f16ac17d8916
James Holt <jamesphilipdavidholt@gmail.com>
2025-12-17 15:40:33 +0000

    html in donate block frequency switch

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.5.4");
+	console.log("v6.5.5");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
