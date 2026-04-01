9e8cee79023abccfe1ea75e0760cbbae62da9a5b
James Holt <jamesphilipdavidholt@gmail.com>
2026-04-01 17:28:42 +0100

    Bump version to 6.6.0 and update donation URL handling in donate block

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.5.9");
+	console.log("v6.6.0");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
