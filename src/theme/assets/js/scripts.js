02cdd729167eb93ca00298c3cdbe07841085d6ec
James Holt <jamesphilipdavidholt@gmail.com>
2026-04-28 11:39:46 +0100

    Bump version to 6.6.1

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.6.0");
+	console.log("v6.6.1");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
