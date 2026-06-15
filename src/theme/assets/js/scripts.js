42000e1ff7790fe0a0ed3ac66cb3705142690718
James Holt <jamesphilipdavidholt@gmail.com>
2026-06-15 11:53:08 +0100

    Add Days of Hope block with SVG graphics and styling
    - Created a new PHP file for the Days of Hope block, including SVG graphics and content.
    - Added SCSS styles for the Days of Hope block, ensuring responsive design and decorative elements.
    - Implemented button styles for donation and fundraising actions, enhancing user engagement.

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.6.9");
+	console.log("v6.7.0");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
