466e9c5fdaf8d381c8c1386ed4faa392177d8dc1
James Holt <jamesphilipdavidholt@gmail.com>
2026-07-03 14:57:02 +0100

    Bump version to 6.7.2 and update console log message

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.7.1");
+	console.log("v6.7.2");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
