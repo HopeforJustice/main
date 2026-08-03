3b0dd9f12cd15bbfd0a1897c815cfc7ae24d9ee3
James Holt <jamesphilipdavidholt@gmail.com>
2026-08-03 09:40:18 +0100

    Bump version to 6.7.4 and remove double message from Days of Hope block

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -35,7 +35,7 @@ function captureUTMParameters() {
 
 /* Page load scripts */
 jQuery(document).ready(function ($) {
-	console.log("v6.7.2");
+	console.log("v6.7.4");
 	captureUTMParameters();
 	let cookies = Cookies.get("wordpress_hfjcookies");
 
